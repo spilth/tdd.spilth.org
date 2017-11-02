a99f8351fe05d9af37bbb5fc2920a1ac5d52a0d3
Brian Kelly <polymonic@gmail.com>
2017-11-01 21:33:34 -0400

    Add S3 Sync and fix build process

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-syntax'
+gem 'middleman-s3_sync'
+gem 'mime-types'
 
--- config.rb
+++ config.rb
@@ -1,5 +1,14 @@
+activate :directory_indexes
 activate :syntax, line_numbers: true
 
+ignore "/language.html"
+
 data.languages.each do |language|
-  proxy "#{language.path}/", '/language.html', locals: { language: language }, layout: :layout
+  proxy "/#{language.path}/index.html", '/language.html', locals: { language: language }, layout: :layout
 end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket = 'tdd.spilth.org'
+  s3_sync.acl    = 'public-read'
+end
+
